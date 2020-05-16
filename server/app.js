15b9393adf7454715fa9e1125098c2ea552689dd
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-16 22:59:33 +0530

    creating a root query
    resolve fn needs to be handled

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express();  // invoking app
 
 app.use('/graphql', graphqlHTTP({
-    
+    schema // <--- es6 syntax <schema: schema>
 }));
 
 app.listen(4000, () => {
