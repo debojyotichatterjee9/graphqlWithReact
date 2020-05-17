408b40d918e886fe640effd134436326f10f886a
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-17 21:01:39 +0530

    adding component and css styles :boom:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div id="main">
-      
-    </div>
-  );
+import BookList from './components/BookList'
+
+class App extends React.Component {
+    render() {
+        return ( 
+            <div id="main">
+            <h1>Heading</h1>
+            <BookList></BookList>
+            </div>
+        );
+    }
 }
 
 export default App;
