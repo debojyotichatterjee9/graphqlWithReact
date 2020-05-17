3adc16505ee80890c08967359fc588fa5668530a
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-17 16:46:06 +0530

    added mongoose to dependency :heavy_plus_sign: and connected to mlab :card_file_box:
    :card_file_box:

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql')
-const schema = require('./schema/schema')
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();  // invoking app
+
+// connecting to mlab database
+mongoose.connect(`mongodb://admin:${encodeURIComponent('admin@123')}@ds263172.mlab.com:63172/graphqldb`);
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to Mlab Database')
+})
 
 app.use('/graphql', graphqlHTTP({
     schema, // <--- es6 syntax <schema: schema>
