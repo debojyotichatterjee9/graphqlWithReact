8e7b2fb6141824ca3824d55c570d259145f8f876
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-17 21:37:49 +0530

    rendering the book get req

--- frontend/src/components/BookList.js
+++ frontend/src/components/BookList.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+{
+    books {
+        name
+        id
+    }
+}
+`
 
 class BookList extends React.Component {
+    displayBooks() {
+        let data = this.props.data;
+        if(data.loading) {
+            return(<div> Loading Data...</div>)
+        }
+        else {
+                        return data.books.map(book => <li key={book.id}>{book.name}</li>)
+                    }
+    }
     render() {
         return ( 
             <div>
                 <ul id="book-list">
-                    <li>Book Name</li>
+                    {this.displayBooks()}
                 </ul>
             </div>
         );
     }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();  // invoking app
+
+
+// allowing cross-origin reqs
+app.use(cors());
 
 // connecting to mlab database
 mongoose.connect(`mongodb://admin:${encodeURIComponent('admin@123')}@ds263172.mlab.com:63172/graphqldb`);
