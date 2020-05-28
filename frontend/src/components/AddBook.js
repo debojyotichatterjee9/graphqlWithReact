61e733178cff7e79a516ac439a70941f02494496
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2020-05-28 22:40:05 +0530

    externalizing queries to a separate file

--- frontend/src/components/AddBook.js
+++ frontend/src/components/AddBook.js
@@ -1,15 +1,6 @@
 import React from 'react';
-import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
-
-const getAuthorsQuery = gql `
-{
-    authors {
-        name
-        id
-    }
-}
-`
+import { getAuthorsQuery } from "../gqlQueries/queries";
 
 
 class AddBook extends React.Component {
